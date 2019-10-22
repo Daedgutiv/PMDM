0d144859fd8221328ab01de4e72ff51cded9ed3a
Daedgutiv <daavogutis@gmail.com>
2019-10-22 21:28:28 +0200

    p2 en casa

---
+++ P2/Areahabitacion/app/src/main/java/com/example/areahabitacion/ui/ui/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.areahabitacion.ui.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.areahabitacion.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calcula(View X){
+        TextView result = (TextView) this.findViewById(R.id.result);
+        EditText largo = (EditText) this.findViewById(R.id.num1);
+        EditText ancho = (EditText) this.findViewById(R.id.num2);
+
+        try {
+            int resultado = Integer.parseInt(largo.getText().toString())*Integer.parseInt(ancho.getText().toString());
+            result.setText(String.valueOf(resultado));
+        } catch(NumberFormatException fmt){
+            result.setText(R.string.error);
+        }
+
+    }
+}
