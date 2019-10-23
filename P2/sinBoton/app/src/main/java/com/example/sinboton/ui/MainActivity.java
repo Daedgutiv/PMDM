00da7ad157b9d31b71269c6174823a22bc49c07b
Daedgutiv <daavogutis@gmail.com>
2019-10-23 19:42:35 +0200

    asdasdasd

--- P2/sinBoton/app/src/main/java/com/example/sinboton/ui/MainActivity.java
+++ P2/sinBoton/app/src/main/java/com/example/sinboton/ui/MainActivity.java
@@ -1,4 +1,51 @@
 package com.example.sinboton.ui;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.area1.ui.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView result;
+    private EditText largo;
+    private  EditText ancho;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        largo = (EditText) this.findViewById(R.id.num1);
+        largo.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                MainActivity.this.calcula();
+                return false;
+            }
+        });
+        ancho = (EditText) this.findViewById(R.id.num2);
+        ancho.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                MainActivity.this.calcula();
+                return false;
+            }
+        });
+    }
+
+    public void calcula(){
+        result = (TextView) this.findViewById(R.id.result);
+        try {
+            int resultado = Integer.parseInt(largo.getText().toString())*Integer.parseInt(ancho.getText().toString());
+            result.setText(String.valueOf(resultado));
+        } catch(NumberFormatException fmt){
+            result.setText(R.string.error);
+        }
+        return;
+    }
 }
